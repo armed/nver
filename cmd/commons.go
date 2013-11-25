d02d126ea562be10e0a6654f9e26bd81377d81e3
Artem Medeusheyev <artem.medeu@gmail.com>
2013-11-25 18:03:13 +0600

    added new method to find directory by version in cmd/commons

--- cmd/commons.go
+++ cmd/commons.go
@@ -1,11 +1,35 @@
 package cmd
 
 import (
+	"github.com/armed/nver/util"
 	"log"
+	"strings"
 )
+
+type searchResults struct {
+	path      string
+	version   string
+	isCurrent bool
+}
 
 func validateArgsNum(args []string, num int) {
 	if len(args) < num {
 		log.Fatal("Not enough arguments")
 	}
+}
+
+func findVersionFolder(verArg string, vList util.VersionList) (result *searchResults, err error) {
+	bestMatch, err := vList.FindExact(verArg)
+	if err != nil {
+		return
+	}
+
+	result = &searchResults{}
+	result.version = bestMatch
+	if result.isCurrent = strings.HasSuffix(bestMatch, "*"); result.isCurrent {
+		result.path = "/current"
+	} else {
+		result.path = "/" + bestMatch
+	}
+	return result, err
 }
